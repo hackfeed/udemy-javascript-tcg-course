6198ca898cf8279e076b13dfc4f0dfbd0de49285
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-02 20:51:16 +0300

    Learn about $ strings

---
+++ 01_basics/assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult += 10;
+
+let calculationDescription = `${defaultResult} + 10`;
+
+outputResult(currentResult, calculationDescription);
