435a2baa07fd634faf3c53c89f47efe01faf14d7
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-04 10:04:12 +0300

    Decompose logic in app.js

--- 01_basics/assets/scripts/app.js
+++ 01_basics/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  outputResult(currentResult, "");
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener("click", add);
