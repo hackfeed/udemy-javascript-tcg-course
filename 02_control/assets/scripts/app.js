136695428d53dff03511e1ef1149f2ff59b0df83
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-06 22:06:14 +0300

    Add base for Monster Killer

---
+++ 02_control/assets/scripts/app.js
@@ -0,0 +1,14 @@
+const ATTACK_VALUE = 10;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+}
+
+attackBtn.addEventListener("click", attackHandler);
