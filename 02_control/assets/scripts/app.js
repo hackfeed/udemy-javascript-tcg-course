a6aefb3c7f377e333e09692175ed64e5c70fe472
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-07 19:42:17 +0300

    Add bonus life handling

--- 02_control/assets/scripts/app.js
+++ 02_control/assets/scripts/app.js
@@ -6,12 +6,23 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
+
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    alert("You would be dead but the bonus life saved you!");
+    setPlayerHealth(initialPlayerHealth);
+  }
+
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("You won!");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
