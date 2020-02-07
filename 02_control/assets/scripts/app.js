4fc69d29e747f0f39b7f0bf1ec01a342c63d8ba7
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-07 20:25:06 +0300

    Utilize hardcode => constants

--- 02_control/assets/scripts/app.js
+++ 02_control/assets/scripts/app.js
@@ -3,6 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACK = "ATTACK";
+const MODE_STRONG_ATTACK = "STRONG_ATTACK";
+
 const enteredMaxLife = prompt("Maximum life for you and the monster", "100");
 
 let chosenMaxLife = parseInt(enteredMaxLife);
@@ -51,7 +54,7 @@ function endRound() {
 
 function attackMonster(mode) {
   let maxDamage;
-  if (mode === "ATTACK") {
+  if (mode === MODE_ATTACK) {
     maxDamage = ATTACK_VALUE;
   } else {
     maxDamage = STRONG_ATTACK_VALUE;
@@ -62,11 +65,11 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-  attackMonster("ATTACK");
+  attackMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler() {
-  attackMonster("STRONG_ATTACK");
+  attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
