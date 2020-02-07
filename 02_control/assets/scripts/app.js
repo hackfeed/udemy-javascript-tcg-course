e8253a69a2bf6cc706e653106af7991ff2eb28c5
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-07 20:22:55 +0300

    Add prompting

--- 02_control/assets/scripts/app.js
+++ 02_control/assets/scripts/app.js
@@ -3,7 +3,14 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredMaxLife = prompt("Maximum life for you and the monster", "100");
+
+let chosenMaxLife = parseInt(enteredMaxLife);
+
+if (isNaN(chosenMaxLife)) {
+  chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
