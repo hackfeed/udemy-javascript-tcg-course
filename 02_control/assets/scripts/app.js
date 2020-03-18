48f9c4f88df07443206009ad6adfce2724f74931
Sergey Kononenko <hackfeed@yandex.ru>
2020-03-18 14:14:56 +0300

    Add exception handling

--- 02_control/assets/scripts/app.js
+++ 02_control/assets/scripts/app.js
@@ -11,13 +11,27 @@ const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
 const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
 const LOG_EVENT_GAME_OVER = "GAME_OVER";
 
-const enteredMaxLife = prompt("Maximum life for you and the monster", "100");
-
-let chosenMaxLife = parseInt(enteredMaxLife);
 let battleLog = [];
 
-if (isNaN(chosenMaxLife)) {
+function getMaxLifeValues() {
+  const enteredValue = prompt("Maximum life for you and the monster", "100");
+  const parsedValue = parseInt(enteredValue);
+
+  if (isNaN(parsedValue) || parsedValue <= 0) {
+    throw { message: "Invalid user input, not a number!" };
+  }
+
+  return parsedValue;
+}
+
+let chosenMaxLife;
+
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+  console.log(error);
   chosenMaxLife = 100;
+  alert("You entered something wrong, default value of 100 was used.");
 }
 
 let currentMonsterHealth = chosenMaxLife;
