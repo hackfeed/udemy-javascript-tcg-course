52a9c9472dc9214ef21888bedbd64e8de4a7ba3d
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-09 02:36:32 +0300

    Add default parameters handling

--- 04_functions/app.js
+++ 04_functions/app.js
@@ -16,7 +16,7 @@ const getPlayerChoice = () => {
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
 
-    return DEFAULT_USER_CHOICE;
+    return;
   }
 
   return selection;
@@ -34,7 +34,7 @@ const getComputerChoice = () => {
   }
 };
 
-const getWinner = (cChoice, pChoice) =>
+const getWinner = (cChoice, pChoice=DEFAULT_USER_CHOICE) =>
   cChoice === pChoice
     ? RESULT_DRAW
     : (cChoice === ROCK && pChoice === PAPER) ||
@@ -53,7 +53,13 @@ const start = startGameBtn.addEventListener("click", () => {
 
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
-  const winner = getWinner(computerChoice, playerChoice);
+  const winner;
+
+  if (playerChoice) {
+    winner = getWinner(computerChoice, playerChoice)
+  } else {
+    winner = getWinner(computerChoice, playerChoice)
+  }
 
   let message = `You picked ${playerChoice}, computer picked ${computerChoice}, therefore you `;
   if (winner === RESULT_DRAW) {
