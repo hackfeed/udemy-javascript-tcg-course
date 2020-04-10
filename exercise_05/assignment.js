3d505408b190c8ae651a678041863f72fb84539d
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-10 15:32:28 +0300

    Add exercise_05 solution

---
+++ exercise_05/assignment.js
@@ -0,0 +1,12 @@
+const firstTaskGetById = document.getElementById("task-1");
+const firstTaskQuerySelector = document.querySelector("#task-1");
+firstTaskQuerySelector.style.background = "black";
+firstTaskQuerySelector.style.color = "white";
+
+const title = document.querySelector("title");
+title.textContent = "Assignment - Solved!";
+const titleOnHead = document.head.querySelector("title");
+titleOnHead.textContent = "Again Solved!";
+
+const h1 = document.body.querySelector("h1");
+h1.textContent = "Assignment - Solved!";
