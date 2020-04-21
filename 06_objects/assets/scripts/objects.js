527d2f6fdb4c9ed4b96af9272f974d9c2ba2d520
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-21 19:33:42 +0300

    Introduction to this keyword

--- 06_objects/assets/scripts/objects.js
+++ 06_objects/assets/scripts/objects.js
@@ -3,6 +3,35 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
+const renderMovies = (filter = "") => {
+  const movieList = document.getElementById("movie-list");
+
+  if (!movies.length) {
+    movieList.classList.remove("visible");
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  for (const movie of filteredMovies) {
+    const movieEl = document.createElement("li");
+    const { info } = movie;
+    // const { getFormattedTitle } = movie;
+    let text = movie.getFormattedTitle() + " - ";
+    for (const key in info) {
+      if (key !== "title") {
+        text += `${key}: ${info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  }
+};
+
 const addMovieHandler = () => {
   const title = document.getElementById("title").value.trim();
   const extraName = document.getElementById("extra-name").value.trim();
@@ -18,10 +47,19 @@ const addMovieHandler = () => {
       [extraName]: extraValue,
     },
     id: Math.random(),
+    getFormattedTitle: function () {
+      return this.info.title.toUpperCase();
+    },
   };
 
   movies.push(newMovie);
-  console.log(movies);
+  renderMovies();
+};
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value.trim();
+  renderMovies(filterTerm);
 };
 
 addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
