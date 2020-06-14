fdcc0bb5f96b078671a3f302067dd9587476255f
Sergey Kononenko <hackfeed@yandex.ru>
2020-06-14 21:34:53 +0300

    Add modules section

---
+++ 15_modules/assets/scripts/Utility/Analytics.js
@@ -0,0 +1,7 @@
+const intervalId = setInterval(() => {
+  console.log("Sending analytics data...");
+}, 2000);
+
+document.getElementById("stop-analytics-btn").addEventListener("click", () => {
+  clearInterval(intervalId);
+});
