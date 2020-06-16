74d1e9a1b10364a2a8a0f70255464dbc1a74f8d0
Sergey Kononenko <hackfeed@yandex.ru>
2020-06-16 23:54:13 +0300

    Add production webpack config

--- 16_tooling/webpack.config.prod.js
+++ 16_tooling/webpack.config.prod.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app.js",
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "assets", "scripts"),
   },
-  devtool: "cheap-module-eval-source-map",
+  devtool: "cheap-source-map",
 };
